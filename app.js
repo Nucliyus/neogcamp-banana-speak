31d8dcfa20719a2cfd3c3d51d17d4616c9854795
Prajwal Chandak <prajwalchandak7@gmail.com>
2021-10-01 18:56:49 +0530

    input output processing done

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
 
-
-console.log(txtInput)
-
 function clickEventHandler() {
-    console.log("clicked!");
-    console.log("input", txtInput.value);
-};
+    outputDiv.innerText = "aajajajajaj " + txtInput.value
+}
 
 btnTranslate.addEventListener("click", clickEventHandler)
 
